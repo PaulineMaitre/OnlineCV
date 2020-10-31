b08a8d1f865265bd6acd078a491af1cc5bd81eb3
PaulineMaitre <pauline.maitre@epfedu.fr>
2020-10-31 17:02:33 +0100

    export JSON works

--- front/src/app/front-office/cv-header/cv-header.component.ts
+++ front/src/app/front-office/cv-header/cv-header.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../../models/User';
+import {of} from 'rxjs';
+
+declare var require: any
+const FileSaver = require('file-saver');
 
 @Component({
   selector: 'app-cv-header',
@@ -10,12 +14,48 @@ export class CvHeaderComponent implements OnInit {
 
   constructor() { }
   @Input() user: User;
+  private setting = {
+    element: {
+      dynamicDownload: null as HTMLElement
+    }
+  }
 
   ngOnInit(): void {
   }
-  clickEvent(): void {
-    console.log('HELLO');
-    console.log(this.user);
+  downloadResumePDF(firstName: string, lastName: string): void {
+    const urlPdf = '../../../assets/CV_' + firstName + '_' + lastName+ '.pdf';
+    const documentName = 'CV_' + firstName + '_' + lastName;
+    FileSaver.saveAs(urlPdf, documentName);
+  }
+
+  getJsonUser() {
+    return of({
+      user: this.user
+    });
+  }
+
+  downloadResumeJSON(firstName: string, lastName: string): void {
+    this.getJsonUser().subscribe((res) =>
+    this.dynamicDownloadByHtmlTag({
+      fileName: 'CV_' + firstName + '_' + lastName + '.json',
+      text: JSON.stringify(res, null, 4)
+    }))
+  }
+
+  private dynamicDownloadByHtmlTag(arg: {
+    fileName: string,
+    text: string
+  }) {
+    if (!this.setting.element.dynamicDownload) {
+      this.setting.element.dynamicDownload = document.createElement('a');
+    }
+    const element = this.setting.element.dynamicDownload;
+    const fileType = arg.fileName.indexOf('.json') > -1 ? 'text/json' : 'text/plain';
+    element.setAttribute('href', `data:${fileType}; charset=utf-8,${encodeURIComponent(arg.text)}`);
+    element.setAttribute('download', arg.fileName);
+
+    const event = new MouseEvent('click');
+    element.dispatchEvent(event);
   }
 
 }
