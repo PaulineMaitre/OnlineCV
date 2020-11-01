da6e7ea6d8df54be47a6b45ab44bf3ed2d57a383
Fabien SOLDE <fabien.solde@epfedu.fr>
2020-11-01 16:45:39 +0100

    FormArray back pour les FrameContents

--- front/src/app/back-office/edit/main-list/frame-content-form/frame-content-form.component.ts
+++ front/src/app/back-office/edit/main-list/frame-content-form/frame-content-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormArray} from '@angular/forms';
 import {UserService} from '../../../../services/user.service';
 import {Network} from '../../../../models/Network';
 import {Skill} from '../../../../models/Skill';
@@ -16,31 +16,72 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 })
 
 export class FrameContentFormComponent implements OnInit {
-  // user: User;
+
   frameContentForm: FormGroup;
   contentForm = [
     'Episode I - The Phantom Menace',
     // 'Episode II - Attack of the Clones',
     // 'Episode III - Revenge of the Sith',
   ];
-
   constructor(private fb: FormBuilder, private userService: UserService) {}
 
-  @Input() title: string;
+  // @Input() title: string;
   @Input() user: User;
 
   ngOnInit(): void {
-    // this.getUser();
-    // setTimeout(() => {}, 2000);
     this.createFrameContentForm();
   }
-
   createFrameContentForm() {
     this.frameContentForm = this.fb.group({
-      title: [this.title],
-      logo: [''],
+      title: ['', Validators.required],
+      logo: ['', Validators.required],
+      frameItem: this.fb.array([])
     });
   }
+
+  get frameItemForm() {
+    return this.frameContentForm.get('frameItem') as FormArray
+  }
+  //
+  // get title() {
+  //   return this.frameItemForm.get('title');
+  // }
+  // get logoItem() {
+  //   return this.frameContentForm.get('frameItem').get('logoItem');
+  // }
+  // get titleItem() {
+  //   return this.frameContentForm.get('frameItem').get('titleItem');
+  // }
+  // get period() {
+  //   return this.frameContentForm.get('frameItem').get('period');
+  // }
+  // get location() {
+  //   return this.frameContentForm.get('frameItem').get('location');
+  // }
+  // get logo() {
+  //   return this.frameContentForm.get('frameItem').get('logo');
+  // }
+  // get content() {
+  //   return this.frameContentForm.get('frameItem').get('content');
+  // }
+
+
+  addFrameItem () {
+    const frame = this.fb.group({
+      titleItem: ['', Validators.required],
+      period: ['', Validators.required],
+      location: ['', Validators.required],
+      logoItem: ['', Validators.required],
+      content: ['', Validators.required],
+        })
+    this.frameItemForm.push(frame)
+  }
+
+  deleteFrameItem(i) {
+    this.frameItemForm.removeAt(i)
+  }
+
+
 
   // getUser(): void {
   //   this.userService.getUserById(1).subscribe(data => {
