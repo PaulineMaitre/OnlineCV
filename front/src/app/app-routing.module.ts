7f2458ef401c3ad8c8cdc43aa9c0a4193ed817c8
Raphaël Della Seta <raphael.dellaseta@epfedu.fr>
2020-11-01 19:13:59 +0100

    ajout d'un truc mais c'est possiblement moche

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -6,14 +6,18 @@ import {FrontOfficeComponent} from './front-office/front-office.component';
 import {MainListComponent} from './back-office/edit/main-list/main-list.component';
 import {EditComponent} from './back-office/edit/edit.component';
 import {HomeComponent} from './home/home.component';
+import {NewUserComponent} from './new-user/new-user.component';
 
 
 // TODO : Ajouter les routes Front/autre
 const routes: Routes = [
-  {path: 'back/:id', component: BackOfficeComponent},
-  {path: 'back/edit/:id', component: EditComponent},
   {path: '', component: HomeComponent},
   {path: ':id', component: FrontOfficeComponent},
+  {path: 'back/:id', component: BackOfficeComponent},
+  {path: 'back/edit/:id', component: EditComponent},
+  {path: 'user/new', component: NewUserComponent},
+
+
 ];
 
 @NgModule({
--- front/src/app/new-user/new-user.component.spec.ts
+++ front/src/app/new-user/new-user.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavComponent } from './nav.component';
+import { NewUserComponent } from './new-user.component';
 
-describe('NavComponent', () => {
-  let component: NavComponent;
-  let fixture: ComponentFixture<NavComponent>;
+describe('NewUserComponent', () => {
+  let component: NewUserComponent;
+  let fixture: ComponentFixture<NewUserComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ NewUserComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavComponent);
+    fixture = TestBed.createComponent(NewUserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
